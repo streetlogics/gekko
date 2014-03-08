bd32fc7e607abc4f3a6d9c68170e8d13986dcb4e
Brad Wedell <brad@mycnl.com>
2014-03-08 14:14:33 -0700

    fixed default custom.js method with loading of log, added stoploss method, updated cex to output error if it's returned from the api

--- methods/stoploss.js
+++ methods/stoploss.js
@@ -7,39 +7,49 @@
 // The example below is pretty stupid: on every new candle there is
 // a 10% chance it will recommand to change your position (to either
 // long or short).
-
-
 // helpers
+var _ = require('lodash');
 var log = require('../core/log.js');
 
 var config = require('../core/util.js').getConfig();
-var settings = config.custom;
+var settings = config.stoploss;
 
 // Let's create our own method
 var method = {};
 
 // Prepare everything our method needs
 method.init = function() {
-  this.name = 'The gambler';
+  this.name = 'Stop Loss';
 
-  this.currentTrend = 'long';
+  this.currentTrend = null;
   this.requiredHistory = 0;
 }
 
 // What happens on every new candle?
 method.update = function(candle) {
+  /* do nothing here */
+  if(this.lastPrice < settings.sell_price) {
+    if(this.currentTrend != 'short'){
+      // If it was long, set it to short
+      this.currentTrend = 'short';
+      this.advice('short');
+    }else{
+      this.advice();
+    }
 
-  // Get a random number between 0 and 1.
-  this.randomNumber = Math.random();
-
-  // There is a 10% chance it is smaller than 0.1
-  this.toUpdate = randomNumber < 0.1;
+  } else {
+    if(this.currentTrend != 'long'){
+      // If it was long, set it to short
+      this.currentTrend = 'long';
+      this.advice('long');
+    }else{
+      this.advice();
+    }
+  }
 }
 
 // For debugging purposes.
 method.log = function() {
-  log.debug('calculated random number:');
-  log.debug('\t', this.randomNumber.toFixed(3));
 }
 
 // Based on the newly calculated
@@ -47,23 +57,6 @@ method.log = function() {
 // update or not.
 method.check = function() {
 
-  // Only continue if we have a new update.
-  if(!this.toUpdate)
-    return;
-
-  if(this.currentTrend === 'long') {
-
-    // If it was long, set it to short
-    this.currentTrend = 'short';
-    this.advice('short');
-
-  } else {
-
-    // If it was short, set it to long
-    this.currentTrend = 'long';
-    this.advice('long');
-
-  }
 }
 
 module.exports = method;
